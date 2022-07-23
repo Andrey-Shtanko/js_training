30ffb78b529212a1922624bdecb7e243a4d3d393
Andrey-Shtanko <shtankoandrew90@gmail.com>
2022-07-23 14:04:07 +0300

    выпы

--- js/example.js
+++ js/example.js
@@ -1,18 +1,58 @@
-// const name = 'Mango';
-// console.log(name.split(' ')); // ["M", "a", "n", "g", "o"]
+// Потрібно створити функцію яка буде приймати 2 параметра.
+// 1)Масив студентів.
+// 2)Ту мову програмування яку потрібно знайти.
+// 3)Функція повинна повертати масив імен які вивчають цю мову.
 
-// const message = 'JavaScript это интересно';
-// console.log(message.split(' ')); // ["JavaScript", "это", "интересно"]
-//
-//
-function count(countFrom = 0, countTo = 10, step = 1) {
-  console.log(`countFrom = ${countFrom}, countTo = ${countTo}, step = ${step}`);
+const students = [
+  {
+    name: `Lika`,
+    langunage: `html`,
+  },
+  {
+    name: `Anton`,
+    langunage: `css`,
+  },
+  {
+    name: `Andriy`,
+    langunage: `js`,
+  },
+  {
+    name: `Vova`,
+    langunage: `html`,
+  },
+  {
+    name: `Alina`,
+    langunage: `css`,
+  },
+  {
+    name: `Egor`,
+    langunage: `js`,
+  },
+  {
+    name: `Aleksandr`,
+    langunage: `html`,
+  },
+  {
+    name: `Taras`,
+    langunage: `css`,
+  },
+  {
+    name: `Ivan`,
+    langunage: `js`,
+  },
+  {
+    name: `Make`,
+    langunage: `html`,
+  },
+];
+const fnA = function (array, lang) {
+  const newArr = [];
 
-  for (let i = countFrom; i <= countTo; i += step) {
-    console.log(i);
+  for (const { name, langunage } of array) {
+    if (langunage === lang) {
+      newArr.push(name);
+    }
   }
-}
-
-count(1, 5); // countFrom = 1, countTo = 5, step = 1
-count(2); // countFrom = 2, countTo = 10, step = 1
-count(); // countFrom = 0, countTo = 10, step = 1
+  return newArr;
+};
+console.log(fnA(students, 'js'));
